55c72caa89b02b784e7dd0a47f1fafdd62745313
Warren Richardson <richarwa@gmail.com>
2020-04-15 18:07:11 -0400

    First commit.

--- plot4.R
+++ plot4.R
@@ -11,6 +11,13 @@ projData <- projData[projData$Date == "1/2/2007"|projData$Date == "2/2/2007",]
 projData <- mutate(projData,datetime = dmy(Date,tz="")+hms(Time))
 
 png(filename = "plot4.png")
+par(mfcol = c(2,2))
+## Top left plot
+plot(projData$datetime,projData$Global_active_power, xlab = "",
+     ylab = "Global Active Power (kilowatts)", type = "n")
+lines(projData$datetime,projData$Global_active_power)
+
+## Bottom left plot
 plot(projData$datetime,projData$Sub_metering_1, xlab = "",
      ylab = "Energy sub metering", type = "n")
 lines(projData$datetime,projData$Sub_metering_1,col = "black")
@@ -19,4 +26,15 @@ lines(projData$datetime,projData$Sub_metering_3,col = "blue")
 legend("topright",
        legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
        lty = 1, col = c("black","red","blue"))
+
+## Top right plot
+plot(projData$datetime,projData$Voltage, xlab = "datetime",
+     ylab = "Voltage", type = "n")
+lines(projData$datetime,projData$Voltage)
+
+## Bottom right plot
+plot(projData$datetime,projData$Global_reactive_power, xlab = "datetime",
+     ylab = "Global_reactive_power", type = "n")
+lines(projData$datetime,projData$Global_reactive_power)
+
 dev.off()
